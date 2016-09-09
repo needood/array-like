ea0c9e3be01e3017138a4c4a5f1f5a41748455ad
yuhaoming <tamamaspace@gmail.com>
2016-09-09 15:10:52 +0800

    init

---
+++ utils.js
@@ -0,0 +1,9 @@
+var each = require("./src/core/each");
+var isArray = require("./src/core/isArray");
+var likeArray = require("./src/core/likeArray");
+module.exports= {
+    each:each,
+    isArray:isArray,
+    likeArray:likeArray,
+};
+
